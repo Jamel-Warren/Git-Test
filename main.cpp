3046164da1197119667f394b0dd1c5496e271241
Jamel-Warren <45469858+Jamel-Warren@users.noreply.github.com>
2018-12-04 12:37:05 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,23 @@
 
 using namespace std;
 
+int sum(int n);
+
 int main()
 {
   cout << "Hello World" << endl;
+  cout << "User needs a number n: ";
+  int n;
+  cin >> n;
+  cout << sum(n) << endl;
+  
   return 0;
+}
+
+int sum(int n)
+{
+  int total=0;
+  for(int i=0; i<n; i++)
+    total = total+(i+1);
+  return total;
 }
